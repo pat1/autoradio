ed569e1328a03bdd62f112c8511824463861e71b
pat1 <pat1@48fb51d1-4e05-4ba3-a40b-24bfba98f144>
2010-03-02 23:19:08 +0000

    updated start istructions

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 # init db and set admin
-django-admin.py syncdb --settings=settings --pythonpath=autoradio
+./autoradioctrl --syncdb
 
 # create locale files
 #django-admin.py makemessages --settings=settings --pythonpath=autoradio -a
 #django-admin.py compilemessages --settings=settings --pythonpath=autoradio
 
 # start autoradiod daemon
-./autoradiod stop
-./autoradiod start
+./autoradiod restart
 
 # run django web server on port 8080
-django-admin.py runserver --settings=settings --pythonpath=autoradio 8080
+./autoradioweb restart
+

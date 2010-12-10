28d78610ed25646cac8ab0f176dec376ea32e1c2
pat1 <pat1@48fb51d1-4e05-4ba3-a40b-24bfba98f144>
2010-12-10 15:39:04 +0000

    doc upgrade

--- branches/merge-prog_pali_podc/setup.py
+++ branches/merge-prog_pali_podc/setup.py
@@ -180,12 +180,22 @@ setup(name='autoradio',
       license = "GNU GPL v2",
       requires= [ "mutagen","django","reportlab"],
       long_description="""\ 
-AutoRadio Radio automation software. 
-Simple to use, starting from digital audio files manages on-air broadcasting over a radio-station or web-radio. 
-The main components are: 
-Player (Xmms): plays all your media files and send digital sound to an audio device or audio server; 
-Scheduler: real time manager for emission of special audio files like jingles, spots, playlist and programs; interact wi
-User inteface: WEB interface to monitor the player and scheduler and admin the schedules for the complete control over y
+Radio automation software. Simple to use, starting from digital audio
+files, manage on-air broadcasting over a radio-station or
+web-radio. The main components are:
+
+    * Player (Xmms/Audacious): plays all your media files and send digital sound
+      to an audio device or audio server
+ 
+    * Scheduler: real time manager for emission of special audio files
+      like jingles, spots, playlist and programs; interact with player
+      like supervisor User
+
+    * inteface: WEB interface to monitor the player and scheduler and
+      admin the schedules for the complete control over your station
+      format. The web interface allows you to easily publish podcasts
+      that conform to the RSS 2.0 and iTunes RSS podcast
+      specifications
 """
      )
      

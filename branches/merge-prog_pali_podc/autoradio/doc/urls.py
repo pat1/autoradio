b2fa6a32703740a804f30f9b0c5a493068a4a1a4
pat1 <pat1@48fb51d1-4e05-4ba3-a40b-24bfba98f144>
2011-07-29 09:57:04 +0000

    start to write documetantion

--- branches/merge-prog_pali_podc/autoradio/doc/urls.py
+++ branches/merge-prog_pali_podc/autoradio/doc/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('autoradio.doc.views',
-    (r'^$', 'index'),
-    (r'^([^/]*)/$', 'doc_reader'),
+    (r'^$', direct_to_template , {'template' : 'doc/index.html'}),
+    (r'^(?P<docitem>\w+)/$', direct_to_template , {'template' : 'doc/doc.html'}),
 )
--- branches/merge-prog_pali_podc/autoradio/doc/views.py
+++
@@ -1,9 +0,0 @@
-from django.template import RequestContext
-from django.views.generic.simple import direct_to_template
-from django.http import Http404
-
-def index(request):
-    return direct_to_template(request, 'doc/index.html')
-
-def doc_reader(request, docname):
-    return direct_to_template(request, 'doc/doc.html', dict(doc=docname))
